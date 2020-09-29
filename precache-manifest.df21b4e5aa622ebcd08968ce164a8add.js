2c09c2de9cb7381734633c42194b9d45c81a6363
chiraag-kakar <chirucos1998@gmail.com>
2020-09-29 17:27:26 +0530

    Updates

---
+++ precache-manifest.df21b4e5aa622ebcd08968ce164a8add.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e031e721168938d0479d5074f48f3f2",
+    "url": "/gstaker/index.html"
+  },
+  {
+    "revision": "01c127b0d29753e0b730",
+    "url": "/gstaker/static/css/main.3d460a0c.chunk.css"
+  },
+  {
+    "revision": "915b4f76ceff7ec65059",
+    "url": "/gstaker/static/js/2.39de797e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gstaker/static/js/2.39de797e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c127b0d29753e0b730",
+    "url": "/gstaker/static/js/main.a9821ed1.chunk.js"
+  },
+  {
+    "revision": "df00f2fafc9629cf24fb",
+    "url": "/gstaker/static/js/runtime-main.199d90f6.js"
+  }
+]);
